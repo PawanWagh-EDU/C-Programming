630d64bae37d198edaea06f1ce8b0018aa0a2ad3
unknown <pawanbwagh@gmail.com>
2025-06-11 17:41:59 +0530

    findMax

---
+++ max.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int a,b;
+    printf("Enter any two no");
+    scanf("%d%d",&a,&b);
+
+    if(a>b){
+        printf("The max no is:- %d");
+    }
+
+    else{
+        printf("The max no is %d");
+    }
+    return 0;
+}
