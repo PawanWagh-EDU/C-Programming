d2d73dc661a113ea8104e65896a6d32a187206ab
Pawan Wagh <pawanbwagh@gmail.com>
2025-06-13 19:07:59 +0530

    Update divisible.c

--- divisible.c
+++ divisible.c
@@ -14,4 +14,18 @@ int main(){
         printf("The no is not divisible");
     }
     return 0;
+
+     //---Using && operator---
+
+    printf("Enter the No :- ");
+    scanf("%d", &a);
+    if (a % 5 == 0 && a % 7 == 0)
+    {
+        printf("The no is divisible  by 5 and 7");
+    }
+    else
+    {
+        printf("The no is not divisible");
+    }
+    return 0;
 }
