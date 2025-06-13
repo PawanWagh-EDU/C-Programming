7659a0c7e0aeb922ca6d02971bd795d369eb9c6f
unknown <pawanbwagh@gmail.com>
2025-06-13 19:00:16 +0530

    Divisible 5or7

--- divisible.c
+++ divisible.c
@@ -2,7 +2,16 @@
 int main(){
     int a;
 
-    printf("Enter the No ;- ");
-    scanf("%d",a);
-    
+    printf("Enter the No :- ");
+    scanf("%d",&a);
+    if(a % 5== 0){
+        printf("The no is divisible  by 5");
+    }
+    else if(a % 7==0){
+        printf("The no is divisible  by 7");
+    }
+    else{
+        printf("The no is not divisible");
+    }
+    return 0;
 }
