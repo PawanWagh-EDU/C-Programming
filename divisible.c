d64af7a89b652e71529dfa9e0be0cb682dba663d
unknown <pawanbwagh@gmail.com>
2025-06-13 19:16:11 +0530

    Between No

--- divisible.c
+++ divisible.c
@@ -1,16 +1,34 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int a;
 
     printf("Enter the No :- ");
-    scanf("%d",&a);
-    if(a % 5== 0){
+    scanf("%d", &a);
+    if (a % 5 == 0)
+    {
         printf("The no is divisible  by 5");
     }
-    else if(a % 7==0){
+    else if (a % 7 == 0)
+    {
         printf("The no is divisible  by 7");
     }
-    else{
+    else
+    {
+        printf("The no is not divisible");
+    }
+    return 0;
+
+    //---Using && operator---
+
+    printf("Enter the No :- ");
+    scanf("%d", &a);
+    if (a % 5 == 0 && a % 7 == 0)
+    {
+        printf("The no is divisible  by 5 and 7");
+    }
+    else
+    {
         printf("The no is not divisible");
     }
     return 0;
