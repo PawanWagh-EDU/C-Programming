3b3be3fbe48ef7e2fb514c570d20fbf29b2a5272
unknown <pawanbwagh@gmail.com>
2025-06-21 11:20:29 +0530

    noWords

--- switchCase.c
+++ switchCase.c
@@ -1,4 +1,48 @@
-#include<stdio.h>
-int  main(){
-    
+#include <stdio.h>
+int main()
+{
+    int a;
+
+    printf("enter the NO to get spelling :- ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+
+    case 1:
+        printf("one");
+        break;
+    case 2:
+        printf("Two");
+        break;
+    case 3:
+        printf("Three");
+        break;
+    case 4:
+        printf("Four");
+        break;
+    case 5:
+        printf("Five");
+        break;
+    case 6:
+        printf("Six");
+        break;
+    case 7:
+        printf("Seven");
+        break;
+    case 8:
+        printf("Eight");
+        break;
+    case 9:
+        printf("Nine");
+        break;
+    case 10:
+        printf("Ten");
+        break;
+
+    default:
+        printf("Enter Value less than 11");
+
+        return 0;
+    }
 }
