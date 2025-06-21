d7865e3dc2790ee93d67b3a55ef991519322ee27
unknown <pawanbwagh@gmail.com>
2025-06-21 12:18:56 +0530

    calsi2

---
+++ switchCalsi.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    char op;
+    
+    printf("Enter any two no :-");
+    scanf("%d %c %d", &a , &op, &b);
+
+    switch (op){
+        case '+' :
+        printf("%d + %d = %d",a,b(a+b));
+        break ;
+
+         case '-' :
+        printf("%d - %d = %d",a,b(a-b));
+        break ;
+
+         case '/' :
+        printf("%d / %d = %d",a,b(a/b));
+        break ;
+
+         case '*' :
+        printf("%d * %d = %d",a,b(a*b));
+        break ;
+
+        default :
+        printf("invalid Opertor");
+
+    }
+}
