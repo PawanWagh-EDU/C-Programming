3c1ee2a6e36f9b05c03b178d93ac79fdbbf9b938
unknown <pawanbwagh@gmail.com>
2025-06-27 11:05:46 +0530

    100 to 150

---
+++ 100loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int n,i;
+
+    printf("Enter the No :-");
+    scanf("%d",&n);
+
+    for(i=100;i<=n;i++){
+        printf("\n %d",i);
+    }
+    return 0;
+
+}
--- helloLoop.c
+++ helloLoop.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main(){
     int n,i;
+    {
+        printf("Hello Pawan");
+    }
+    return 0;
+
+}
+
+//----hello N-times-------
+
+#include <stdio.h>
+int main(){
+    int n,i;
 
     printf("Enter the No :-");
     scanf("%d",&n);
